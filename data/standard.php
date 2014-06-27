04ca31ede12aa67ad45d5795846e1040e9c60f90
Limonclib <limonclib@openmailbox.org>
2014-06-27 14:32:58 +0200

    Mise à jour du lien LICENSE.txt
    Mise à jour du lien pour une meilleure intégration.

--- data/standard.php
+++ data/standard.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Witzing Copyright (C) 2014 Rémi Duplé sous les termes de la license GNU GPL version 3 (voir le fichier "licence.txt")
+Witzing Copyright (C) 2014 Rémi Duplé sous les termes de la license GNU GPL version 3 (voir le fichier "LICENSE.txt")
 */
 $regex_hashtag='A-Z0-9éèàêëâä\-_à€';
 $photos_url='';
